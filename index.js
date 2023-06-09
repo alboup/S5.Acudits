fa039ab2a4c361f2396afebfceb36724d024482e
Albert Bou Pinar <alboup@gmail.com>
2023-06-09 11:38:55 +0200

    Exercici 4
    API meteorològica

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ function votarAcudit(score) {
   }
   console.log(reportAcudits);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    getTiempo();
+  });
+
+async function getTiempo() {
+  const tempsData = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=10f9b5b8eb548e0af3a7418fb76a0d4d&lang=ca&units=metric',
+    {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+  );
+  const tempsObj = await tempsData.json();
+  const location = tempsObj.name;
+  const temperature = tempsObj.main.temp;
+
+  document.getElementById('ubicacion-temperatura').innerHTML = `${location}: ${temperature} ºC`;
+}
